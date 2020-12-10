2ef590da6b2d2453b3ef81ce03eae14b3da97b15
mathieures <mathieuresseguier@gmail.com>
2020-12-10 23:11:36 +0100

    amelioré annuler() pour desactiver les options du menu, modifié Cube.py pour pouvoir dessiner() et effacer() a notre guise.

--- Cube.py
+++ Cube.py
@@ -3,7 +3,7 @@ import Grille
 
 class Cube:
 
-	couleur_haut = "#bfbfbf"
+	couleur_haut = "#afafaf"
 	couleur_gauche = "#808080"
 	couleur_droite = "#414141"
 
@@ -28,17 +28,26 @@ class Cube:
 		return self.__h
 
 	#constructeur
-	def __init__(self,pcanvas,pgrille,pcoordsGrille,phauteur=0):
+	def __init__(self,pcanvas,pgrille,pcoordsGrille,phauteur=0,pcouleur=(couleur_haut,couleur_gauche,couleur_droite)):
 		# pcoords est un point de la grille, (hauteur 0 !!)
-		d = pgrille.definition
 
 		self.coords = pgrille.grilleToCanvas(pcoordsGrille)
 
-		# dessin
+		self.dessiner(pcanvas,pgrille,phauteur,pcouleur)
+
+		self.__id = self.__haut
+
+	# destructeur
+	def __del__(self):
+		# print("destruction d'un cube")
+		return
+
+	def dessiner(self,pcanvas,pgrille,phauteur,pcouleur):
+		d = pgrille.definition
+		
 		x = self.coords[0]
 		y = self.coords[1]
 		self.__h = phauteur
-		print("hauteur :",self.__h)
 
 
 		A = (x-d,y-d/2) # haut gauche
@@ -49,20 +58,12 @@ class Cube:
 		F = (x,y-d) # haut
 		
 		# on ajoute un tag aux faces, pour que quand on clique sur une face, on puisse avoir le cube
-		self.__haut = pcanvas.create_polygon(self.coords,B,F,A,fill='#afafaf',outline="black")
+		self.__haut = pcanvas.create_polygon(self.coords,B,F,A,fill=pcouleur[0],outline="black")
 		tag = "cube_"+str(self.__haut)
 		pcanvas.itemconfig(self.__haut,tags=tag)
 		
-		self.__gauche = pcanvas.create_polygon(self.coords,A,C,E,fill='#414141',outline="black",tags=tag)
-		self.__droite = pcanvas.create_polygon(self.coords,B,D,E,fill='#808080',outline="black",tags=tag)
-
-		# self.dessine(pcanvas, d, self.coords)
-
-		self.__id = self.__haut
-
-	# destructeur
-	def __del__(self):
-		print("destruction d'un cube")
+		self.__gauche = pcanvas.create_polygon(self.coords,A,C,E,fill=pcouleur[1],outline="black",tags=tag)
+		self.__droite = pcanvas.create_polygon(self.coords,B,D,E,fill=pcouleur[2],outline="black",tags=tag)
 
 	def effacer(self,pcanvas):
 		pcanvas.delete(self.__haut,self.__gauche,self.__droite)
--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from tkinter import filedialog,messagebox
 
 class App:
 
+	# DICO : contient la position 3D de tous les cubes sous la forme DICO[(x,y)] = [hauteur_1,hauteur_2...]
+	DICO = {}
+
+	CUBES = [] # liste des cubes places
+
 	def addCubeToDico(self,pcoordsGrille,phauteur):
 		x,y = pcoordsGrille
 		if((x,y) in self.DICO):
@@ -20,8 +25,8 @@ class App:
 		# self.canv.delete("tag_cube") # plus simple a ecrire qu'appeler Cube.effacer() pour tous les cubes
 		self.CUBES = []
 		self.DICO = {}
-		# on desactive l'option de sauvegarde
-		# self.deroulFichier.entryconfigure(2,state="disabled")
+		self.deroulFichier.entryconfigure(2,state="disabled") # option Sauver
+		self.deroulFichier.entryconfigure(3,state="disabled") # option Annuler
 
 	def sauverFichier(self):
 		fichier = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text files", ".txt"),("All files", ".*"))) # On demande a l'utilisateur dans quel fichier il veut sauver le projet
@@ -45,15 +50,15 @@ class App:
 				parse = pos_cube.rstrip().split(" ")
 				pos = parse[0].split(",") # On prend le permier element de parse qui sera la position du cube, les autres elements sont les hauteurs
 				for i in range(1,len(parse)):
-					self.placerCube((int(pos[0]),int(pos[1])),int(parse[i])) 
+					self.placerCube((int(pos[0]),int(pos[1])),int(parse[i]))
 			fichier.close()
 
 	def annulerDernierCube(self,event=None):
 		"""Annule le dernier placement de cube."""
 		# le parametre event est obligatoire pour etre bind
 		if len(self.CUBES) == 1:
-			# on desactive l'option pour annuler
-			self.deroulFichier.entryconfigure(2,state="disabled")
+			self.deroulFichier.entryconfigure(2,state="disabled") # option Sauver
+			self.deroulFichier.entryconfigure(3,state="disabled") # option Annuler
 		if len(self.CUBES) > 0:
 			cube = self.CUBES[-1]
 			coordsGrille = self.grille.canvasToGrille(cube.coords)
@@ -67,47 +72,51 @@ class App:
 
 
 	def placerCube(self,pcoordsGrille,phauteur):
-		"""Prend en parametre des coordonnes de self.grille (peut-etre a changer ?)"""
-
+		"""
+		Prend en parametre des coordonnes de self.grille (peut-etre a changer ?)
+		Attention : recale la position au point le plus proche (closestPoint)
+		"""
 		coords = self.grille.closestPoint(pcoordsGrille)
-		print("coords 3D :",coords,phauteur,end=" ; ")
+
 		self.addCubeToDico(coords,phauteur) # coordonnees 3D
 
 		# on transforme les coordonnees 3D en coordonnees 2D
 		coords = (coords[0]-phauteur,coords[1]-phauteur)
-		print("coords 2D :",coords)
 
-
-		print("  placed cube at :",coords)
 		cube = Cube.Cube(self.canv,self.grille,coords,phauteur)
-		# on a la hauteur en cliquant sur une face du haut
-		# il faut passer les coordonnees du cube mais avec la hauteur +1
+		print("  placed cube",cube.id,"at :",(*pcoordsGrille,phauteur),"("+str(coords)+")") # coords 3D puis 2D
+
 		self.CUBES.append(cube)
 		self.deroulFichier.entryconfigure(2,state="active")
 		self.deroulFichier.entryconfigure(3,state="active")
-		return cube # sert a rien mais au cas ou
+		return cube # ne sert a rien pour l'instant, mais au cas ou
 
 	def placerCubeHaut(self,pposition3D):
+		"""Place un cube en haut de la position 3D passee en parametre (x,y,hauteur)"""
 		x,y,h = pposition3D
-		self.placerCube((x,y),h+1)
+		return self.placerCube((x,y),h+1)
 
 	def placerCubeGauche(self,pposition3D):
+		"""Place un cube a gauche de la position 3D passee en parametre (x,y,hauteur)"""
 		x,y,h = pposition3D
-		self.placerCube((x,y+1),h)
+		return self.placerCube((x,y+1),h)
 
 	def placerCubeDroite(self,pposition3D):
+		"""Place un cube a droite de la position 3D passee en parametre (x,y,hauteur)"""
 		x,y,h = pposition3D
-		self.placerCube((x+1,y),h)
+		return self.placerCube((x+1,y),h)
 
 
 	def onClick(self,event):
+		"""En cas de clic sur le canvas"""
+
 		currentCoords = (event.x,event.y)
-		convertedCoords = self.grille.canvasToGrille((currentCoords))
-		print("click on :",currentCoords,"=>",convertedCoords)
+		convertedCoords = self.grille.canvasToGrille(currentCoords)
+		# print("click on :",currentCoords,"=>",convertedCoords)
 
 		faceCliquee = self.canv.find_withtag("current") # id du polygone sur lequel on a clique
-		if(self.canv.type(faceCliquee) == "polygon"):
-
+		
+		if(self.canv.type(faceCliquee) == "polygon"): # si on a bien clique sur un polygone (une face de cube)
 			idCube = int(self.canv.gettags(faceCliquee)[0].split("_")[1]) # tag 0 : "cube_idfaceduhaut"
 			for cube in self.CUBES: # on teste chaque cube deja place
 				if cube.id == idCube: # si c'est celui sur lequel on a clique
@@ -117,32 +126,30 @@ class App:
 
 					# on teste les id de faces pour savoir laquelle c'etait ; faceCliquee[0] car c'est un tuple
 					if faceCliquee[0] == cube.haut:
-						print("on place le cube sur la face haut")
 						self.placerCubeHaut(position3D)
 					elif faceCliquee[0] == cube.gauche:
-						print("on place le cube sur la face gauche")
 						self.placerCubeGauche(position3D)
 					elif faceCliquee[0] == cube.droite:
-						print("on place le cube sur la face droite")
 						self.placerCubeDroite(position3D)
 					break
-		
-		# sinon on a clique sur la grille
+	
+		# sinon ce n'est pas un polygone alors on a clique autre part : on regarde si c'est dans la grille
 		elif convertedCoords[0] < self.grille.taille_x and convertedCoords[1] < self.grille.taille_y and convertedCoords[0] > 0 and convertedCoords[1] > 0:
 			# il faut supprimer la limite de placement en vertical vers le haut, sinon on peut pas faire + de 1 cube en (0,0)
 			hauteur = 0
-			self.placerCube((convertedCoords[0]-0.5,convertedCoords[1]-0.5),hauteur) # on ajuste le point 0.5 case plus haut
+			self.placerCube(self.grille.closestPointUp(convertedCoords),hauteur) # on ajuste le point 0.5 case plus haut
 
 	def onMotion(self,event):
+		"""
+		En cas mouvement dans le canvas.
+		"""
 		currentCoords = (event.x,event.y)
-		convertedCoords = self.grille.canvasToGrille((currentCoords))
-		print("click on :",currentCoords,"=>",convertedCoords)
-		self.placerCube(convertedCoords)
+		convertedCoords = self.grille.canvasToGrille(currentCoords)
+
+		return
+
 
 	def __init__(self):
-
-		self.CUBES = [] # liste des cubes places
-
 
 		# Root
 		self.root = tk.Tk()
@@ -193,13 +200,11 @@ class App:
 
 		self.canv = tk.Canvas(self.root,width=500,height=500,bg="white")
 		self.canv.bind("<Button-1>",self.onClick)
+		self.canv.bind("<Motion>",self.onMotion)
 
 		self.grille = Grille.Grille(self.canv)
 
 		# cube1 = Cube.Cube(self.canv,self.grille,(1,1))
-
-		# DICO
-		self.DICO = {}
 
 		self.canv.pack()
 		# print("is ready :",isReadyToDraw)
